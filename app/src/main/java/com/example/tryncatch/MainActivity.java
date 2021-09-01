ebb715aaccd9a6ff827ce0eca97d600c1974d391
Imam Ghazi Khan <igkhan1710@gmail.com>
2021-09-01 23:07:52 +0530

    Implemented the ClickListeners and removed separate functions for each websites and rather collected them in an String array for cleaner code

--- app/src/main/java/com/example/tryncatch/MainActivity.java
+++ app/src/main/java/com/example/tryncatch/MainActivity.java
@@ -1,18 +1,18 @@
 package com.example.tryncatch;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
+
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
@@ -21,39 +21,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = findViewById(R.id.weblist);
-        String arr[] = {"GeeksForGeeks","LeetCode","InterviewBit","CodeForces","CodeChef"};
-        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_2,arr);
+        String websites[] = {"GeeksForGeeks", "LeetCode", "InterviewBit", "CodeForces", "CodeChef","HackerRank","HackerEarth","GitHub","FreeCodeCamp","W3School","CodingBat"};
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, websites);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                String selectedWebsite = adapterView.getItemAtPosition(position).toString();
+                Toast.makeText(MainActivity.this, "Opening "+selectedWebsite, Toast.LENGTH_SHORT).show();
+                    String weblinks[] = {"https://geeksforgeeks.org", "https://leetcode.com", "https://interviewbit.com", "https://codechef.com", "https://codeforces.com","https://hackerrank.com","https://hackerearth.com","https://github.com","https://freecodecamp.org","https://w3schools.com","https://codingbat.com"};
+                    Uri link = Uri.parse(weblinks[position]);
+                    Intent webIntent = new Intent(Intent.ACTION_VIEW, link);
+                    startActivity(webIntent);
+            }
+        });
     }
-
-    public void gfg(View view)
-    {
-        Uri gfglink = Uri.parse("https://www.geeksforgeeks.org");
-        Intent gfgIntent = new Intent(Intent.ACTION_VIEW, gfglink);
-        startActivity(gfgIntent);
     }
-    public void leetCode(View view)
-    {
-        Uri leetcodelink = Uri.parse("https://www.leetcode.com");
-        Intent leetIntent = new Intent(Intent.ACTION_VIEW,leetcodelink);
-        startActivity(leetIntent);
-    }
-    public void interviewBit(View view)
-    {
-        Uri interviewbitlink = Uri.parse("https://www.interviewbit.com");
-        Intent interviewbitIntent = new Intent(Intent.ACTION_VIEW,interviewbitlink);
-        startActivity(interviewbitIntent);
-    }
-    public void codeChef(View view)
-    {
-        Uri codeCheflink = Uri.parse("https://www.codechef.com");
-        Intent codeChefIntent = new Intent(Intent.ACTION_VIEW,codeCheflink);
-        startActivity(codeChefIntent);
-    }
-    public void codeForces(View view)
-    {
-        Uri codeForceslink = Uri.parse("https://codeforces.com");
-        Intent codeForcesIntent = new Intent(Intent.ACTION_VIEW,codeForceslink);
-        startActivity(codeForcesIntent);
-    }
-}
