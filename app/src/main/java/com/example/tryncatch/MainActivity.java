b1fa9b423feb1112e74388eef6b7ca02f3fd689d
Imam Ghazi Khan <igkhan1710@gmail.com>
2021-09-01 21:47:15 +0530

    Created a list that will direct to their respective websites,and defined function for each list. Haven't implemented their onClick attributes yet tho

--- app/src/main/java/com/example/tryncatch/MainActivity.java
+++ app/src/main/java/com/example/tryncatch/MainActivity.java
@@ -1,19 +1,59 @@
 package com.example.tryncatch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Uri webpage = Uri.parse("https://www.geeksforgeeks.org");
-        Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
-        startActivity(webIntent);
+        listView = findViewById(R.id.weblist);
+        String arr[] = {"GeeksForGeeks","LeetCode","InterviewBit","CodeForces","CodeChef"};
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_2,arr);
+        listView.setAdapter(adapter);
+    }
+
+    public void gfg(View view)
+    {
+        Uri gfglink = Uri.parse("https://www.geeksforgeeks.org");
+        Intent gfgIntent = new Intent(Intent.ACTION_VIEW, gfglink);
+        startActivity(gfgIntent);
+    }
+    public void leetCode(View view)
+    {
+        Uri leetcodelink = Uri.parse("https://www.leetcode.com");
+        Intent leetIntent = new Intent(Intent.ACTION_VIEW,leetcodelink);
+        startActivity(leetIntent);
+    }
+    public void interviewBit(View view)
+    {
+        Uri interviewbitlink = Uri.parse("https://www.interviewbit.com");
+        Intent interviewbitIntent = new Intent(Intent.ACTION_VIEW,interviewbitlink);
+        startActivity(interviewbitIntent);
+    }
+    public void codeChef(View view)
+    {
+        Uri codeCheflink = Uri.parse("https://www.codechef.com");
+        Intent codeChefIntent = new Intent(Intent.ACTION_VIEW,codeCheflink);
+        startActivity(codeChefIntent);
+    }
+    public void codeForces(View view)
+    {
+        Uri codeForceslink = Uri.parse("https://codeforces.com");
+        Intent codeForcesIntent = new Intent(Intent.ACTION_VIEW,codeForceslink);
+        startActivity(codeForcesIntent);
     }
 }
